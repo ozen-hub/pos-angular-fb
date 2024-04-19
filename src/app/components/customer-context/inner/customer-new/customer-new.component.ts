85b5e302ed71c6e651c49dff1013b78cda029b02
ozen-hub <info.ozenhub@gmail.com>
2024-04-19 21:21:05 +0530

    2

--- src/app/components/customer-context/inner/customer-new/customer-new.component.ts
+++ src/app/components/customer-context/inner/customer-new/customer-new.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-customer-new',
@@ -10,11 +11,22 @@ import {MatButton} from "@angular/material/button";
     MatFormField,
     MatLabel,
     MatInput,
-    MatButton
+    MatButton,
+    ReactiveFormsModule
   ],
   templateUrl: './customer-new.component.html',
   styleUrl: './customer-new.component.scss'
 })
 export class CustomerNewComponent {
+  loading:boolean=false;
+  form= new FormGroup({
+    fullName: new FormControl('',[Validators.required]),
+    address: new FormControl('',[Validators.required]),
+    salary: new FormControl('',[Validators.required]),
+    avatar: new FormControl('',[Validators.required])
+  })
 
+  saveCustomer() {
+
+  }
 }
