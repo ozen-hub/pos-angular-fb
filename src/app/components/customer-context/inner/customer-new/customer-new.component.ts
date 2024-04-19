0e5e2ce09589e7239eadba3e5fdc0d55fcba9bb7
ozen-hub <info.ozenhub@gmail.com>
2024-04-19 21:09:57 +0530

    1

--- src/app/components/customer-context/inner/customer-new/customer-new.component.ts
+++ src/app/components/customer-context/inner/customer-new/customer-new.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-customer-new',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatButton
+  ],
   templateUrl: './customer-new.component.html',
   styleUrl: './customer-new.component.scss'
 })
